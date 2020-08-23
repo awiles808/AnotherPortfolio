55e0b9bf2111058aee5d6702bd02f0c270744190
awiles808 <awiles808@gmail.com>
2020-08-23 16:10:49 -0600

    Created React App and Editied Existing Files And Added Header Component

---
+++ anotherportfolio/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>
+      This is Anotha one
+<Header/>
+    </h1>
+    </div>
+  );
+}
+
+export default App;
+ 
