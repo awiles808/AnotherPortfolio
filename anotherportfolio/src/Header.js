0174a3ae5afe4ceb1f7a9f0fa8852a3978005f72
awiles808 <awiles808@gmail.com>
2020-08-24 16:27:21 -0600

    Editing Header With Menu Icon

--- anotherportfolio/src/Header.js
+++ anotherportfolio/src/Header.js
@@ -5,8 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-
+// import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 
@@ -24,6 +23,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function MenuAppBar() {
     const classes = useStyles();
+
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
 
@@ -42,23 +42,18 @@ export default function MenuAppBar() {
           
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                         <MenuIcon />
-                    </IconButton>
+                    </IconButton> */}
                     <Typography variant="h6" className={classes.title}>
                         Photos
           </Typography>
-                   
+                    
                         <div>
-                            <IconButton
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleMenu}
-                                color="inherit"
-                            >
-                                <AccountCircle />
-                            </IconButton>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu"  onClick={handleMenu}>
+                            <MenuIcon />
+                        </IconButton>
+                            
                             <Menu
                                 id="menu-appbar"
                                 anchorEl={anchorEl}
@@ -78,7 +73,7 @@ export default function MenuAppBar() {
                                 <MenuItem onClick={handleClose}>My account</MenuItem>
                             </Menu>
                         </div>
-                  
+                   
                 </Toolbar>
             </AppBar>
         </div>
